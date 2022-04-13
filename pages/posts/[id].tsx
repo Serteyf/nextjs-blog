d920fa3192e5e992bb7267b4a0a4acc620ab9fd2
Federico <Silva>
2022-04-13 19:11:36 -0300

    Add typescript to files

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,29 +1,19 @@
 import Layout from '../../components/layout'
-import Date from '../../components/date'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-
-
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id)
-  return {
-    props: {
-      postData
-    }
-  }
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds()
-  return {
-    paths,
-    fallback: false
-  }
-}
-
+import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
+import { GetStaticProps, GetStaticPaths } from 'next'
 
-export default function Post({ postData }) {
+export default function Post({
+  postData
+}: {
+  postData: {
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
@@ -38,4 +28,21 @@ export default function Post({ postData }) {
       </article>
     </Layout>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string)
+  return {
+    props: {
+      postData
+    }
+  }
 }
